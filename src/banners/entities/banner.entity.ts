3e395ee36fb45c72a0c2dfe79fea9132ff06f6c4
matheusalper <alperzeybek2@gmail.com>
2026-06-26 14:40:30 +0300

    Create banner.entity.ts

---
+++ src/banners/entities/banner.entity.ts
@@ -0,0 +1,55 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity('banners')
+export class Banner {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  title: string;
+
+  @Column({ nullable: true })
+  subtitle: string; // küçük üst başlık / etiket
+
+  @Column({ type: 'text', nullable: true })
+  description: string;
+
+  @Column({ nullable: true })
+  button1Text: string;
+
+  @Column({ nullable: true })
+  button1Link: string;
+
+  @Column({ nullable: true })
+  button2Text: string;
+
+  @Column({ nullable: true })
+  button2Link: string;
+
+  @Column({ nullable: true })
+  desktopImage: string;
+
+  @Column({ nullable: true })
+  tabletImage: string;
+
+  @Column({ nullable: true })
+  mobileImage: string;
+
+  @Column({ default: 0 })
+  sortOrder: number;
+
+  @Column({ default: true })
+  isActive: boolean;
+
+  @Column({ type: 'date', nullable: true })
+  startDate: string;
+
+  @Column({ type: 'date', nullable: true })
+  endDate: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
