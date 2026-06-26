ed5859109419370d6c06a5f6053a4f68b72de111
matheusalper <alperzeybek2@gmail.com>
2026-06-26 15:13:22 +0300

    Update banners.module.ts

--- src/banners/banners.module.ts
+++ src/banners/banners.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BannersController } from './banners.controller';
 import { BannersService } from './banners.service';
+import { ImageProcessingService } from './image-processing.service';
 import { Banner } from './entities/banner.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Banner])],
   controllers: [BannersController],
-  providers: [BannersService],
+  providers: [BannersService, ImageProcessingService],
   exports: [BannersService],
 })
 export class BannersModule {}
