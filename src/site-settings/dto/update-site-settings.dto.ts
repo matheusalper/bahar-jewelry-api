cff98692ab32888a552b927e0c8de1c328de3386
matheusalper <alperzeybek2@gmail.com>
2026-06-26 10:54:57 +0300

    Create update-site-settings.dto.ts

---
+++ src/site-settings/dto/update-site-settings.dto.ts
@@ -0,0 +1,18 @@
+import { IsObject, IsOptional } from 'class-validator';
+
+export class UpdateSiteSettingsDto {
+  @IsObject()
+  @IsOptional()
+  banner?: Record<string, any>;
+
+  @IsOptional()
+  announcementBar?: any[];
+
+  @IsObject()
+  @IsOptional()
+  socialLinks?: Record<string, any>;
+
+  @IsObject()
+  @IsOptional()
+  contactInfo?: Record<string, any>;
+}
