dccfb99b311b41e8ddb5abc789a2068a5d18a3fd
matheusalper <alperzeybek2@gmail.com>
2026-06-26 14:41:07 +0300

    Create create-banner.dto.ts

---
+++ src/banners/dto/create-banner.dto.ts
@@ -0,0 +1,59 @@
+import { IsBoolean, IsDateString, IsInt, IsOptional, IsString, MinLength } from 'class-validator';
+
+export class CreateBannerDto {
+  @IsString()
+  @MinLength(1)
+  title: string;
+
+  @IsString()
+  @IsOptional()
+  subtitle?: string;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @IsString()
+  @IsOptional()
+  button1Text?: string;
+
+  @IsString()
+  @IsOptional()
+  button1Link?: string;
+
+  @IsString()
+  @IsOptional()
+  button2Text?: string;
+
+  @IsString()
+  @IsOptional()
+  button2Link?: string;
+
+  @IsString()
+  @IsOptional()
+  desktopImage?: string;
+
+  @IsString()
+  @IsOptional()
+  tabletImage?: string;
+
+  @IsString()
+  @IsOptional()
+  mobileImage?: string;
+
+  @IsInt()
+  @IsOptional()
+  sortOrder?: number;
+
+  @IsBoolean()
+  @IsOptional()
+  isActive?: boolean;
+
+  @IsDateString()
+  @IsOptional()
+  startDate?: string;
+
+  @IsDateString()
+  @IsOptional()
+  endDate?: string;
+}
