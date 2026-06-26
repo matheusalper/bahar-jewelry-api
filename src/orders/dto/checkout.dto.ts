290a380b552e7c76396aeb20184407f37884d6b8
matheusalper <alperzeybek2@gmail.com>
2026-06-26 22:27:45 +0300

    Update checkout.dto.ts

--- src/orders/dto/checkout.dto.ts
+++ src/orders/dto/checkout.dto.ts
@@ -1,8 +1,10 @@
-import { IsString } from 'class-validator';
+import { IsOptional, IsString } from 'class-validator';
 
 export class CheckoutDto {
   @IsString()
   shippingAddress: string;
 
-  // Faz 5'te ödeme yöntemi seçimi (mock / iyzico) buraya eklenecek
+  @IsString()
+  @IsOptional()
+  paymentMethod?: string;
 }
