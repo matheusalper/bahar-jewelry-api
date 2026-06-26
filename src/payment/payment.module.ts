8caffd2e19791f4883abaaf72c4b6fb6be8974cf
matheusalper <alperzeybek2@gmail.com>
2026-06-26 23:37:04 +0300

    Update payment.module.ts

--- src/payment/payment.module.ts
+++ src/payment/payment.module.ts
@@ -2,9 +2,10 @@ import { Module, forwardRef } from '@nestjs/common';
 import { PaymentController } from './payment.controller';
 import { PaymentService } from './payment.service';
 import { OrdersModule } from '../orders/orders.module';
+import { SiteSettingsModule } from '../site-settings/site-settings.module';
 
 @Module({
-  imports: [forwardRef(() => OrdersModule)],
+  imports: [forwardRef(() => OrdersModule), SiteSettingsModule],
   controllers: [PaymentController],
   providers: [PaymentService],
   exports: [PaymentService],
