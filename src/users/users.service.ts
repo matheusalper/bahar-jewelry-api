59e765c5b6f1098d926fe928832398246f949d82
matheusalper <alperzeybek2@gmail.com>
2026-06-26 22:24:59 +0300

    Update users.service.ts

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import * as bcrypt from 'bcryptjs';
 import { User } from './entities/user.entity';
 
 @Injectable()
@@ -21,5 +22,25 @@ export class UsersService {
   async create(data: { name: string; email: string; password: string }): Promise<User> {
     const user = this.userRepo.create(data);
     return this.userRepo.save(user);
+  }
+
+  async updateProfile(id: string, data: { name?: string; phone?: string }) {
+    const user = await this.userRepo.findOne({ where: { id } });
+    if (!user) throw new BadRequestException('Kullanıcı bulunamadı');
+    Object.assign(user, {
+      name: data.name ?? user.name,
+      phone: data.phone !== undefined ? data.phone : user.phone,
+    });
+    return this.userRepo.save(user);
+  }
+
+  async updatePassword(id: string, currentPassword: string, newPassword: string) {
+    const user = await this.userRepo.findOne({ where: { id } });
+    if (!user) throw new BadRequestException('Kullanıcı bulunamadı');
+    const matches = await bcrypt.compare(currentPassword, user.password);
+    if (!matches) throw new UnauthorizedException('Mevcut şifre hatalı');
+    user.password = await bcrypt.hash(newPassword, 10);
+    await this.userRepo.save(user);
+    return { updated: true };
   }
 }
