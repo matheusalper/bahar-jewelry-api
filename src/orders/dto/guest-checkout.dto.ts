d680f42edde1dbc01aad2f8532702e8c2c97bc58
matheusalper <alperzeybek2@gmail.com>
2026-06-26 09:53:17 +0300

    Create guest-checkout.dto.ts

---
+++ src/orders/dto/guest-checkout.dto.ts
@@ -0,0 +1,31 @@
+import { IsEmail, IsOptional, IsString, MinLength } from 'class-validator';
+
+export class GuestCheckoutDto {
+  @IsString()
+  @MinLength(2)
+  name: string;
+
+  @IsString()
+  @MinLength(7)
+  phone: string;
+
+  @IsEmail()
+  @IsOptional()
+  email?: string;
+
+  @IsString()
+  @MinLength(2)
+  city: string;
+
+  @IsString()
+  @MinLength(2)
+  district: string;
+
+  @IsString()
+  @MinLength(5)
+  address: string;
+
+  @IsString()
+  @IsOptional()
+  note?: string;
+}
