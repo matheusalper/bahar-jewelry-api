89a28ac00c1818ea6e696bad2810fdc42f5c3f95
matheusalper <alperzeybek2@gmail.com>
2026-06-27 10:45:15 +0300

    Update admin.module.ts

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -4,10 +4,11 @@ import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
 import { Order } from '../orders/entities/order.entity';
 import { Product } from '../products/entities/product.entity';
+import { User } from '../users/entities/user.entity';
 import { OrdersModule } from '../orders/orders.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Order, Product]), OrdersModule],
+  imports: [TypeOrmModule.forFeature([Order, Product, User]), OrdersModule],
   controllers: [AdminController],
   providers: [AdminService],
 })
