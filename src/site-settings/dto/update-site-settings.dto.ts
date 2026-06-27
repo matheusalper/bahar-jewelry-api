89326169fd683f78bd31c10129e6cb2e679f65cc
matheusalper <alperzeybek2@gmail.com>
2026-06-27 10:28:57 +0300

    Update update-site-settings.dto.ts

--- src/site-settings/dto/update-site-settings.dto.ts
+++ src/site-settings/dto/update-site-settings.dto.ts
@@ -19,4 +19,8 @@ export class UpdateSiteSettingsDto {
   @IsObject()
   @IsOptional()
   paymentSettings?: Record<string, any>;
+
+  @IsObject()
+  @IsOptional()
+  baharParaSettings?: Record<string, any>;
 }
