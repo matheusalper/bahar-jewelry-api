e85698e6dedd8f1e788ee57dc3bb24040c363395
matheusalper <alperzeybek2@gmail.com>
2026-06-29 10:36:19 +0300

    Update site-settings.module.ts

--- src/site-settings/site-settings.module.ts
+++ src/site-settings/site-settings.module.ts
@@ -3,9 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { SiteSettingsController } from './site-settings.controller';
 import { SiteSettingsService } from './site-settings.service';
 import { SiteSettings } from './entities/site-settings.entity';
+import { Product } from '../products/entities/product.entity';
+import { Category } from '../products/entities/category.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([SiteSettings])],
+  imports: [TypeOrmModule.forFeature([SiteSettings, Product, Category])],
   controllers: [SiteSettingsController],
   providers: [SiteSettingsService],
   exports: [SiteSettingsService],
