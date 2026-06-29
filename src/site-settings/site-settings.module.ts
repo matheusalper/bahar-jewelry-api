aadf229a93a20bc98b36c551c45aee620af8ae64
matheusalper <alperzeybek2@gmail.com>
2026-06-29 15:39:02 +0300

    Update site-settings.module.ts

--- src/site-settings/site-settings.module.ts
+++ src/site-settings/site-settings.module.ts
@@ -5,9 +5,10 @@ import { SiteSettingsService } from './site-settings.service';
 import { SiteSettings } from './entities/site-settings.entity';
 import { Product } from '../products/entities/product.entity';
 import { Category } from '../products/entities/category.entity';
+import { BlogPost } from '../blog/entities/blog-post.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([SiteSettings, Product, Category])],
+  imports: [TypeOrmModule.forFeature([SiteSettings, Product, Category, BlogPost])],
   controllers: [SiteSettingsController],
   providers: [SiteSettingsService],
   exports: [SiteSettingsService],
