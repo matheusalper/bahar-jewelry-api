8e188c7789f5106f563ff24b961bbc12b947e76c
matheusalper <alperzeybek2@gmail.com>
2026-06-29 12:33:43 +0300

    Update admin.module.ts

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -3,12 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
 import { Order } from '../orders/entities/order.entity';
+import { OrderItem } from '../orders/entities/order-item.entity';
 import { Product } from '../products/entities/product.entity';
 import { User } from '../users/entities/user.entity';
 import { OrdersModule } from '../orders/orders.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Order, Product, User]), OrdersModule],
+  imports: [TypeOrmModule.forFeature([Order, OrderItem, Product, User]), OrdersModule],
   controllers: [AdminController],
   providers: [AdminService],
 })
